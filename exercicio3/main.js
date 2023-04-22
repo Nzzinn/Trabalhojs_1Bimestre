b045412e792ba4387ef5e47d910f8c743aa241d5
Nathan Ferreira <107155373+Nzzinn@users.noreply.github.com>
2023-04-22 17:10:45 -0300

    Delete exercicio3 directory

--- exercicio3/main.js
+++
@@ -1,6 +0,0 @@
-function sorteiaNum(min, max, sorteado){
-    min = parseInt(document.getElementById('min').value);
-    max = parseInt(document.getElementById('max').value);
-    sorteado = Math.floor(Math.random() * (max - min) + min);
-    document.getElementById('resultado').innerHTML = `O número sorteado foi: ${sorteado}`;
-};  
